067bbd603b42d67f6a59061563641824e725f9a6
britto vincent <brittovincent@brittos-MacBook-Pro.local>
2026-04-01 13:26:10 +0530

    date changed

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ const lidToJid = new Map();
 const SCHEDULED_MESSAGES = [
   { time: "12:00", text: "da evidya" },
   { time: "12:10", text: "da evidya" },
-  { time: "13:20", text: "da evidya" },
+  { time: "13:30", text: "da evidya" },
+  { time: "13:27", text: "da evidya koii" },
 
   { time: "21:30", text: "nee epo bangalore ano" },
 ];
